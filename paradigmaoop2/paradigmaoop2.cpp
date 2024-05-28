790aae0fc933474fb6aef31fd5150e4e95008c7b
gagatpadi <padiyogaradhana@gmail.com>
2024-05-28 18:19:26 +0700

    Add project files.

---
+++ paradigmaoop2/paradigmaoop2.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+
+
+
+class seseorang {
+public:
+    virtual void pesan() = 0;
+
+
+
+};
+
+class joko :public seseorang {
+public:
+    void pesan() {
+        cout << "Pesan dari joko" << endl;
+    }
+};
+
+class lia :public seseorang {
+public:
+    void pesan() {
+        cout << "Pesan dari Lia" << endl;
+    }
+
+};
+
+int main() {
+    seseorang* obyek;
+    joko a;
+    lia b;
+
+    obyek = &a;
+    obyek->pesan();
+    obyek = &b;
+    obyek->pesan();
+    //a.seseorang::pesan();
+
+    return 0
+
+}
