95872064f3d64a27ef2933d7fb65368a97db00c7
gagatpadi <padiyogaradhana@gmail.com>
2024-05-28 18:20:00 +0700

    commit class seseorang

--- paradigmaoop2/paradigmaoop2.cpp
+++ paradigmaoop2/paradigmaoop2.cpp
@@ -12,32 +12,3 @@ public:
 
 };
 
-class joko :public seseorang {
-public:
-    void pesan() {
-        cout << "Pesan dari joko" << endl;
-    }
-};
-
-class lia :public seseorang {
-public:
-    void pesan() {
-        cout << "Pesan dari Lia" << endl;
-    }
-
-};
-
-int main() {
-    seseorang* obyek;
-    joko a;
-    lia b;
-
-    obyek = &a;
-    obyek->pesan();
-    obyek = &b;
-    obyek->pesan();
-    //a.seseorang::pesan();
-
-    return 0
-
-}
